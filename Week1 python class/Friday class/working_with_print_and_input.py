e3239f0d64944cd6d1c0cdbad820e0f0d7250464
Zedprince <zacheaustaiwo2020@gmail.com>
2025-08-21 15:37:45 +0100

    progrss1

---
+++ Week1 python class/Friday class/working_with_print_and_input.py
@@ -0,0 +1,35 @@
+# Basic usage of print
+print("Hello, world")
+
+# using print with variables
+name="Aisha"
+age=25
+print("Name :", name)
+print("Age :", age)
+print("My name is",name, "I am", age, "years old.")
+
+# Using f-strings with print()
+name= "Bola"
+score=98
+print(f"My name is {name} i scored {score} in my last math exam.")
+
+# Using string concatenation
+first_name="Ada"
+last_name=" Beta"
+print("my name is" +" "+ first_name + " "+ last_name)
+
+# Comma vs concatenation
+print("Hello", "World")      # With comma
+print("Hello"+" "+"World")   # With concatenation
+
+# Escape sequences in print - "\n", "\t"
+print("line1\nline2")
+print("line1\tline2")
+print("He said, \"Hello\" today")
+print("It\'s python!")
+
+# Backslash
+print("File path: c:\\users\\Aisha\\")
+
+# Carriage return (\r)
+print("123456\rABC")  # Output: ABC456 (replaces 123)
